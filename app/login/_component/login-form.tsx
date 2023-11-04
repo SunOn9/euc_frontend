19c7c65da3b1899b6f82cb7d41ca29448b84fc3a
SunOn9 <lehophattai@gmail.com>
2023-11-04 11:37:34 +0700

    commit

--- app/login/_component/login-form.tsx
+++ app/login/_component/login-form.tsx
@@ -1,35 +1,34 @@
 "use client";
-import { Card, CardBody, CardHeader, Input } from "@nextui-org/react";
+import { EyeFilledIcon, EyeSlashFilledIcon } from "@/components/icons";
+import { text } from "@/components/primitives";
+import { Button, Card, CardBody, CardHeader, Input } from "@nextui-org/react";
+import clsx from "clsx";
 import { Formik } from "formik";
+import Link from "next/link";
+import { useState } from "react";
+import * as Yup from "yup";
 
 export default function LoginForm() {
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string().email("Invalid email").required("Required"),
+    password: Yup.string().required("Required"),
+  });
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
+
   return (
-    <Card className="py-4">
-      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-        <h4 className="font-bold text-large">Frontend Radio</h4>
-        <small className="text-default-500">12 Tracks</small>
+    <Card className="p-3 lg:min-w-[30%] md:min-w-[40%]	sm:min-w-[50%]">
+      <CardHeader className="pb-4 pt-2 px-4 flex-col items-start">
+        <h4 className="font-bold text-large">Login to EUC</h4>
+        <small className="text-default-500">Welcome to EUC</small>
       </CardHeader>
       <Formik
         initialValues={{ email: "", password: "" }}
-        validate={(values) => {
-          const errors = {
-            email: "",
-            password: "",
-          };
-          if (!values.email) {
-            errors.email = "Required";
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = "Invalid email address";
-          }
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
+        validationSchema={LoginSchema}
+        onSubmit={(values) => {
+          console.log(values);
         }}
       >
         {({
@@ -43,30 +42,66 @@ export default function LoginForm() {
           /* and other goodies */
         }) => (
           <CardBody className="overflow-visible py-2">
-            <form onSubmit={handleSubmit}>
+            <form
+              className="flex flex-col place-items-center	"
+              onSubmit={handleSubmit}
+            >
               <Input
+                className="pb-5"
                 type="email"
                 label="Email"
                 name="email"
                 onChange={handleChange}
                 onBlur={handleBlur}
                 value={values.email}
-                errorMessage={errors.email && touched.email && errors.email}
+                errorMessage={
+                  errors.email && touched.email ? errors.email : " "
+                }
               />
-
               <Input
-                type="password"
+                className="pb-5 "
+                label="Password"
                 name="password"
+                placeholder="Enter your password "
+                endContent={
+                  <button
+                    className="focus:outline-none	ml-2"
+                    type="button"
+                    onClick={toggleVisibility}
+                  >
+                    {isVisible ? (
+                      <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                    ) : (
+                      <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                    )}
+                  </button>
+                }
+                type={isVisible ? "text" : "password"}
                 onChange={handleChange}
                 onBlur={handleBlur}
                 value={values.password}
                 errorMessage={
-                  errors.password && touched.password && errors.password
+                  errors.password && touched.password ? errors.password : " "
                 }
               />
-              <button type="submit" disabled={isSubmitting}>
+              <Link
+                href={"/"}
+                className={text({
+                  color: "cyan",
+                })}
+              >
+                Forgot your password ?
+              </Link>
+              <Button
+                radius="full"
+                className={clsx(
+                  "bg-gradient-to-tr from-[#FF705B] to-[#FFB457] text-white shadow-lg"
+                )}
+                type="submit"
+                isLoading={isSubmitting}
+              >
                 Submit
-              </button>
+              </Button>
             </form>
           </CardBody>
         )}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,13 @@
-import { title, subtitle } from "@/components/primitives";
+import { title } from "@/components/primitives";
 import LoginForm from "./_component/login-form";
 
 export default function Login() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+    <section className="h-full		 flex flex-col items-center content-evenly	 justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-lg text-center justify-center">
         <h1 className={title({ color: "yellow" })}>Entixie&nbsp;</h1>
         <h1 className={title()}>Ultimate Club&nbsp;</h1>
-        <h2 className={subtitle({ class: "mt-4" })}> Login </h2>
       </div>
-
       <LoginForm />
     </section>
   );
